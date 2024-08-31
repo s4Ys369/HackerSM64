16aa1ec5986495b9a1b1650d7b72ee5197b3e6cd
s4ys369 <twitchversusmarioneat@gmail.com>
2024-08-31 14:04:10 -0400

    update: bhvTouchedStarSpawn

--- levels/ccm/script.c
+++ levels/ccm/script.c
@@ -138,13 +138,13 @@ static const LevelScript script_ccm_area_1_objects_2[] = {
     OBJECT(/*model*/ MODEL_PENGUIN,          /*pos*/  -555,  3470, -1000, /*angle*/ 0,   0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvSmallPenguin),
     OBJECT(/*model*/ MODEL_MR_BLIZZARD,      /*pos*/ -2376, -1589,  4256, /*angle*/ 0, 252, 0, /*behParam*/ 0x00010000, /*beh*/ bhvMrBlizzard),
     OBJECT(/*model*/ MODEL_MR_BLIZZARD,      /*pos*/  -394, -1589,  4878, /*angle*/ 0,  74, 0, /*behParam*/ 0x00010000, /*beh*/ bhvMrBlizzard),
-    OBJECT_WITH_ACTS(/*model*/ MODEL_CCM_SNOWMAN_BASE, /*pos*/  2560,  2662, -1122, /*angle*/ 0,   0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvSnowmansBottom,    /*acts*/ ACT_5),
-    OBJECT_WITH_ACTS(/*model*/ MODEL_NONE,             /*pos*/  -402,  461,  -2898, /*angle*/ 0,   0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvSnowmanMidpoint, /*acts*/ ACT_5),
+    OBJECT_WITH_ACTS(/*model*/ MODEL_CCM_SNOWMAN_BASE, /*pos*/  2560,  2662, -1122, /*angle*/ 0, 0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvSnowmansBottom,  /*acts*/ ACT_5),
+    OBJECT_WITH_ACTS(/*model*/ MODEL_NONE,             /*pos*/  -402,   461, -2898, /*angle*/ 0, 0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvSnowmanMidpoint, /*acts*/ ACT_5),
     RETURN(),
 };
 
 static const LevelScript script_ccm_area_1_objects_3[] = {
-    OBJECT_WITH_ACTS(/*model*/ MODEL_NONE,             /*pos*/  2665, -4607,  4525, /*angle*/ 0,   0, 0, /*behParam*/ 0x0073648D, /*beh*/ bhvTouchedStarSpawn, /*acts*/ ALL_ACTS),
+    OBJECT_WITH_ACTS(/*model*/ MODEL_NONE,             /*pos*/  2665, -4607,  4525, /*angle*/ 0,   0, 0, /*behParam*/ 0x0073647F, /*beh*/ bhvTouchedStarSpawn,    /*acts*/ ALL_ACTS),
     OBJECT_WITH_ACTS(/*model*/ MODEL_NONE,             /*pos*/  2500, -4350,  5750, /*angle*/ 0,   0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvDefaultStarMarker,   /*acts*/ ALL_ACTS),
     OBJECT_WITH_ACTS(/*model*/ MODEL_PENGUIN,          /*pos*/  3450, -4700,  4550, /*angle*/ 0,   0, 0, /*behParam*/ 0x01000000, /*beh*/ bhvTuxiesMother,        /*acts*/ ALL_ACTS),
     OBJECT_WITH_ACTS(/*model*/ MODEL_NONE,             /*pos*/  3167, -4300,  5108, /*angle*/ 0,   0, 0, /*behParam*/ 0x01000000, /*beh*/ bhvDefaultStarMarker,   /*acts*/ ALL_ACTS),
--- src/game/behaviors/spawn_star_exit.inc.c
+++ src/game/behaviors/spawn_star_exit.inc.c
@@ -1,13 +1,19 @@
 // spawn_star_exit.inc.c
 
 void bhv_touched_star_spawn_loop(void) {
-    f32 offsetX = (f32)GET_BPARAM2(o->oBehParams);
-    f32 offsetY = (f32)GET_BPARAM3(o->oBehParams);
-    f32 offsetZ = (f32)GET_BPARAM4(o->oBehParams);
+    /*
+    * There's probably a more efficient way to achieve this,
+    * but this allows for individual axis offsets, range [-128,127]
+    * NOTE: Vanilla uses a Z offset of +141 units, but 14 units is a
+    * worthy sacrifice. This visual difference is almost imperceptible.
+    */
+    s8 offsetX = GET_BPARAM2(o->oBehParams);
+    s8 offsetY = GET_BPARAM3(o->oBehParams);
+    s8 offsetZ = GET_BPARAM4(o->oBehParams);
     if (gEnteredArea2 & 1) {
-        o->oPosX = o->oPosX + offsetX;
-        o->oPosY += offsetY;
-        o->oPosZ = o->oPosZ + offsetZ;
+        o->oPosX = o->oPosX + (f32)offsetX;
+        o->oPosY += (f32)offsetY;
+        o->oPosZ = o->oPosZ + (f32)offsetZ;
         spawn_default_star();
         obj_mark_for_deletion(o);
     }
